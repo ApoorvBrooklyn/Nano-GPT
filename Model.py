1298cdd4711a66fb12c454a14c121ef0aa5e6e33
ApoorvBrooklyn <apoorobot21@gmail.com>
2024-07-10 19:13:55 +0530

    Code has error Needs to be fixed

--- Model.py
+++ Model.py
@@ -1,15 +1,31 @@
 import torch
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+
+
+# Hyper Parameter 
+batch_size = 32
+block_size = 8
+max_iters = 3000
+learning_rate= 1e-2
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+eval_iters = 200
+n_embd = 384
+eval_interval = 500
+
+torch.manual_seed(1337)
+
 
 # Reading the txt
 
 with open('input.txt', 'r', encoding='utf-8') as f:
     text = f.read()
 
-print(len(text))
 
 
 chars = sorted(list(set(text)))
-vocab_Size = len(chars)
+vocab_size = len(chars)
 # print(''.join(chars))
 # print(vocab_Size)
 
@@ -33,57 +49,80 @@ train_data = data[:n]
 val_data = data[n:]
 
 
-block_size = 8
-x = train_data[:block_size]
-y = train_data[1:block_size+1]
+#block_size = 8
+# x = train_data[:block_size]
+# y = train_data[1:block_size+1]
 
-for t in range(block_size):
-    context = x[:t+1]
-    target = y[t]
-    # print(f"When input is {context} the target is {target}")
+# for t in range(block_size):
+#     context = x[:t+1]
+#     target = y[t]
+#     # print(f"When input is {context} the target is {target}")
 
 
-torch.manual_seed(1337)
-batch_size = 4 # Number of independent sequences we will generate
-block_size = 8 # What is maximum context length for prediction
 
+# batch_size = 4 # Number of independent sequences we will generate
+# block_size = 8 # What is maximum context length for prediction
+# Data Loading
 def get_batch(split):
     data = train_data if split == 'train' else val_data
     ix = torch.randint(len(data) - block_size, (batch_size,))
     x = torch.stack([data[i:i+block_size] for i in ix])
     y = torch.stack([data[i+1:i+block_size+1] for i in ix])
+    x,y = x.to(device), y.to(device)
     return x,y
 
-xb, yb = get_batch('train')
-print('inputs: ')
-print(xb.shape)
-print(xb)
-print('targets:')
-print(yb.shape)
-print(yb)
+# xb, yb = get_batch('train')
+# print('inputs: ')
+# print(xb.shape)
+# print(xb)
+# print('targets:')
+# print(yb.shape)
+# print(yb)
 
 
-print("-----------------------")
+# print("-----------------------")
 
-for b in range(batch_size): # Batch Dimension
-    for t in range(block_size): #time dimension
-        context = xb[b, :t+1]
-        target = yb[b,t]
-        print(f"When input is {context.tolist()} the target is: {target}")
+# for b in range(batch_size): # Batch Dimension
+#     for t in range(block_size): #time dimension
+#         context = xb[b, :t+1]
+#         target = yb[b,t]
+#         print(f"When input is {context.tolist()} the target is: {target}")
 
 
-import torch
-import torch.nn as nn
-from torch.nn import functional as F
-torch.manual_seed(1337)
 
+# torch.manual_seed(1337)
+@torch.no_grad() 
+def estimate_loss():
+    out = {}
+    model.eval()
+    for split in ['train', 'val']:
+        losses = torch.zeros(eval_iters)
+        for k in range(eval_iters):
+            X, Y = get_batch(split)
+            logits, loss = model(X, Y)
+            losses[k] = loss.item()
+        out[split] = losses.mean()
+    model.train()
+    return out
+
+
+# Simple Bigram Model
 class BigramLanguageModel(nn.Module):
-    def __init__(self, vocab_size):
+
+
+    def __init__(self): # We can add vocab_size here !! Wait for time being
         super().__init__()
-        self.token_embeddings_table = nn.Embedding(vocab_size, vocab_size)
+        #each token directly reads off the logits for the next token from Lookup table
+        self.token_embeddings_table = nn.Embedding(vocab_size, n_embd)
+        self.position_embeddings_table = nn.Embedding(block_size, n_embd)
+        self.lm_head = nn.Linear(n_embd, vocab_size)
 
     def forward(self, idx, targets=None):
-        logits = self.token_embeddings_table(idx) # Batch, time, Channel tensor
+        B,T = idx.shape
+        tok_emb = self.token_embeddings_table(idx)
+        pos_emb = self.position_embeddings_table(torch.arange(T, device = device))
+        x = tok_emb + pos_emb #(B,T,C)
+        logits = self.lm_head(idx) # (B, T)
         # Here Batch is 4, time is 8, channel is vocab_size = 65
         # loss = F.cross_entropy(logits, targets)
         # Above is actual method but in doc we will find that F.cross expects Channel as Second input here we have it as third 
@@ -115,31 +154,47 @@ class BigramLanguageModel(nn.Module):
 
     
 # Technically Logits are the predictions and BigramLAnguageModel is just simple Neural network with Forward pass    
-m = BigramLanguageModel(vocab_Size)
-logits, loss = m(xb, yb)
-print(logits.shape)
-print(loss)
+model = BigramLanguageModel()
+m = model.to(device)
+# logits, loss = m(xb, yb)
+# print(logits.shape)
+# print(loss)
 # Loss here is 4.87 which can be estimated by -ln(1/65) ie -ln(1/vocab_size) which is around 4.17
 
 ## We are creating a tensor of (1,1) dimension
 # idx = torch.zeros((1,1), dtype = torch.long)
 ## Since the generate works on batches we have to unpluck single batchwise elements which gives us Time Steps which are converted into List using tolist() function
-print(decode(m.generate(idx = torch.zeros((1,1), dtype = torch.long), max_new_tokens=100)[0].tolist()))  
+# print(decode(m.generate(idx = torch.zeros((1,1), dtype = torch.long), max_new_tokens=100)[0].tolist()))  
 
 
-optimizer = torch.optim.AdamW(m.parameters(), lr=1e-3)
+optimizer = torch.optim.AdamW(m.parameters(), lr=learning_rate)
 
-batch_size = 32
-for steps in range(5000):
-    xb,yb = get_batch('train')
+for iter in range(max_iters):
+    # For every fe interval we will evaluate model
+    if iter % eval_interval == 0:
+        losses = estimate_loss()
+        print(f"step {iter}: train loss{losses['train']:.4f}, val loss {losses['val']:.4f}")
 
-    # Evaluate Loss
-    logits, loss = m(xb, yb)
+    # Sample of data
+    xb, yb = get_batch('train')
+
+    # Evaluate loss
+    logits, loss = model(xb, yb)
     optimizer.zero_grad(set_to_none=True)
     loss.backward()
     optimizer.step()
 
-    print(loss.item())
 
-print(decode(m.generate(idx = torch.zeros((1,1), dtype = torch.long), max_new_tokens=400)[0].tolist()))  
+# for steps in range(5000):
+#     xb,yb = get_batch('train')
+
+#     # Evaluate Loss
+#     logits, loss = m(xb, yb)
+#     optimizer.zero_grad(set_to_none=True)
+#     loss.backward()
+#     optimizer.step()
+
+#     print(loss.item())
+context = torch.zeros((1,1), dtype=torch.long, device = device)
+print(decode(m.generate(context, max_new_tokens=500)[0].tolist()))
 
